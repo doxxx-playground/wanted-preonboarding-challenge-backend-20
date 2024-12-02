5cb0b172377401c662809ab233ef19c92ae199b5
doxxx93 <doxxx93@gmail.com>
2024-12-02 12:33:59 +0900

    chore: add spring security dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,14 +23,14 @@ repositories {
     mavenCentral()
 }
 
-extra["springModulithVersion"] = "1.3.0"
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     compileOnly("org.projectlombok:lombok")
